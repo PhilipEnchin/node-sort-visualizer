c53a988431c523b9edf1bad06a1d5dde15884d71
Philip Enchin <Philip@Ench.in>
2021-04-03 02:15:42 +0000

    Restructure functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,31 +1,23 @@
-var sort = function (arr, frameMaker) {
-
-};
-
-var makeFrame = function (arr) {
-
-};
-
-module.exports = {
-  makeCountingArray: function (len) {
-    var arr = Array(len);
-    for (var i = 0; i < len; i++) {
-      arr[i] = i + 1;
-    }
-
-    return arr;
-  },
-  makeShuffledArray: function (len) {
-    var arr = module.exports.makeCountingArray(len);
-    for (var i = 1; i < len; i++) {
-      var j = module.exports.randomUpTo(i);
-      var temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    return arr;
-  },
-  randomUpTo: function (upper) {
-    return Math.floor(Math.random() * (upper + 1));
+module.exports.makeCountingArray = function (len) {
+  var arr = Array(len);
+  for (var i = 0; i < len; i++) {
+    arr[i] = i + 1;
   }
-}
+
+  return arr;
+};
+
+module.exports.makeShuffledArray = function (len) {
+  var arr = module.exports.makeCountingArray(len);
+  for (var i = 1; i < len; i++) {
+    var j = module.exports.randomUpTo(i);
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+
+module.exports.randomUpTo = function (upper) {
+  return Math.floor(Math.random() * (upper + 1));
+};
