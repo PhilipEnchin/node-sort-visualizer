7bce551aef0bddc7a9f28ee91bad6b4887016a9b
Philip Enchin <Philip@Ench.in>
2021-04-03 20:46:56 +0000

    Add function to reset all stubs, fix bug

--- test/stub.js
+++ test/stub.js
@@ -13,8 +13,13 @@ module.exports.unstub = function (obj, funcName) {
   for (var i = 0; i < stubs.length; i++) {
     if (stubs[i].obj === obj && stubs[i].funcName === funcName) {
       obj[funcName] = stubs[i].func;
+      stubs = stubs.slice(0, i).concat(stubs.slice(i + 1));
       return;
     }
   }
   throw new Error('Did not find ' + funcName + ' in stubs for ' + obj);
+};
+
+module.exports.resetStubs = function () {
+  while (stubs.length) module.exports.unstub(stubs[0].obj, stubs[0].funcName)
 };
